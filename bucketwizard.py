84df731c6dd69d56be5eca75041dcbd68a736292
bbramanti <bbramanti@gmail.com>
2019-06-11 23:34:33 -0500

    added pyinquirer

--- bucketwizard.py
+++ bucketwizard.py
@@ -1,18 +1,17 @@
 import requests
-from bs4 import BeautifulSoup
 from teams import teams
+from bs4 import BeautifulSoup
+from PyInquirer import prompt, print_json
+from pprint import pprint
 
-def main():
-	print("BucketWizard at attention ...\n")
-	print("Which team's stats would you like to view?\n")
-
-	user_input = input()
-	selected_team = ""
+def get_abbrev(team):
 	for key,value in teams.items():
-		if (user_input in value):
-			selected_team = key
+		if value == team:
+			return key
 
-	url = "https://www.basketball-reference.com/teams/"+selected_team+"/2019.html"
+def collectData(team, year):
+	selected_team = get_abbrev(team)
+	url = "https://www.basketball-reference.com/teams/"+selected_team+"/"+year+".html"
 	page = requests.get(url)
 	page_found = False 
 
@@ -20,13 +19,41 @@ def main():
 		print("page data secured ...")
 		page_found = True 
 		soup = BeautifulSoup(page.content, 'html.parser')
+
 	elif (page.status_code == 404):
 		print("page not found, spelling of team may be incorrect")
 
 	if (page_found):
-		print ("which stat in specific?")
+		## use soup to access html content
+		print ("continue with soup object here")
 
+def get_teams(answers):
+	options = []
+	for key,value in teams.items():
+		options.append(value)
+	return options
+		
+def main():
+	print("BucketWizard at attention ...\n")
 
+	questions = [
+    	{
+        'type': 'list',
+        'name': 'team_name',
+        'message': 'What team\'s data would you like to view?',
+        'choices': get_teams,
+    	},
+    	{
+        'type': 'list',
+        'name': 'team_year',
+        'message': 'Which year?',
+        'choices': ['2015','2016','2017','2018','2019'],
+    	}
+
+	]
+	answers = prompt(questions)
+	pprint(answers)
+	collectData(answers['team_name'], answers['team_year'])
 
 
 if __name__ == "__main__":
