9600d54b560f2cf9e11b91a6ccd8fc0061370a01
bbramanti <bbramanti@gmail.com>
2019-06-20 01:51:02 -0500

    added tabulate package to print out data in tabular form

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 Running this application will require the following packages:
 - requests (https://pypi.org/project/requests/)
-- bs4 (https://pypi.org/project/beautifulsoup4/)
+- bs4 (https://www.crummy.com/software/BeautifulSoup/bs4/doc/)
 - PyInquirer (https://github.com/CITGuru/PyInquirer/)
+- Tabulate (https://bitbucket.org/astanin/python-tabulate/src/master/)
--- bucketwizard.py
+++ bucketwizard.py
@@ -3,6 +3,7 @@ import re
 from teams import teams
 from bs4 import BeautifulSoup
 from PyInquirer import prompt
+from tabulate import tabulate
 
 def get_abbrev(team):
 	for key,value in teams.items():
@@ -18,33 +19,51 @@ def get_salary(html):
 			td = tr.find_all('td')
 			data = [x.text for x in td]
 			salaries.append(data)
+		# remove empty header row
 		salaries = salaries[1:]
-		print (salaries) 
+		# final print to user
+		print()
+		print(tabulate(salaries, headers=["Player", "Salary"], tablefmt="github"))
+		print()
 	else:
 		print ("cannot retrieve table")
 
 def collect_data(team, year, selection):
 	selected_team = get_abbrev(team)
+	# if 2015-2016, grab 2016
 	selected_year = year[5:]
 	url = "https://www.basketball-reference.com/teams/"+selected_team+"/"+selected_year+".html"
 	page = requests.get(url)
-	page_found = False 
 
 	if (page.status_code == 200):
-		print("page data secured ...")
-		page_found = True 
 		# decode so we can use regex to remove commenting
 		html = page.content.decode('utf-8')
 		soup = BeautifulSoup(re.sub("<!--|-->","", html), 'html.parser')
+		running = True
+		while (running):
+			# view salary data
+			if (selection == 'Player Salaries'):
+				get_salary(soup)
+
+			# confirm if user wants to view more of this team's stats
+			questions = [
+				{
+        		'type': 'confirm',
+        		'message': 'Continue viewing ' + team + ' stats?',
+        		'name': 'continue',
+        		'default': True,
+    			}]
+			answers = prompt(questions)
+			if (answers['continue'] == False):
+				# exit
+				running = False
+			# else: user makes stat selection again
+
+		print ("bucketwizard shutting down ...")
+		# option here to allow user to cycle back to main()
 
 	elif (page.status_code == 404):
 		print("page not found, spelling of team may be incorrect")
-
-	if (page_found):
-		# view salary data
-		if (selection == 'Player Salaries'):
-			get_salary(soup)
-
 
 def main():
 	questions = [
@@ -65,12 +84,9 @@ def main():
         'name': 'stat_selection',
         'message': 'Which data would you like to view?',
         'choices': ['Player Salaries', 'PPG', 'Rebounds']
-    	}
-
-	]
+    	}]
 	answers = prompt(questions)
 	collect_data(answers['team_name'], answers['team_year'], answers['stat_selection'])
-
 
 if __name__ == "__main__":
 	main()
