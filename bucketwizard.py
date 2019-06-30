3b3db2147cffc65d3155edf5a345c5c8e433416d
bbramanti <bbramanti@gmail.com>
2019-06-29 19:22:16 -0500

    added option to view team roster, flipped key/value in teams json

--- bucketwizard.py
+++ bucketwizard.py
@@ -6,12 +6,6 @@ from PyInquirer import prompt
 from tabulate import tabulate
 
 
-def get_abbrev(team):
-    for key, value in teams.items():
-        if value == team:
-            return key
-
-
 def get_salary(html):
     table = html.find(id="salaries2")
     salaries = []
@@ -32,8 +26,40 @@ def get_salary(html):
         print ("cannot retrieve table")
 
 
+def get_roster(html):
+    table = html.find(id="roster")
+    roster = []
+    if (table):
+        # get table headers
+        header = table.find('thead')
+        header_row = header.find('tr')
+        headers = []
+        for th in header_row.find_all('th'):
+            # check for &nbsp using strip()
+            if (th.text.strip()):
+                headers.append(th.text)
+            else:
+                # instead of &nbsp
+                headers.append("Birth Country")
+        # get table data
+        rows = table.find_all('tr')
+        for tr in rows:
+            td = tr.find_all(['td', 'th'])
+            data = [x.text for x in td]
+            roster.append(data)
+        # remove empty header row
+        roster = roster[1:]
+        # final print to user
+        print()
+        print(tabulate(
+            roster, headers=headers, tablefmt="github"))
+        print()
+    else:
+        print ("cannot retrieve table")
+
+
 def collect_data(team, year, selection):
-    selected_team = get_abbrev(team)
+    selected_team = teams[team]
     # if 2015-2016, grab 2016
     selected_year = year[5:]
     url = "https://www.basketball-reference.com/teams/"
@@ -49,6 +75,8 @@ def collect_data(team, year, selection):
             # view salary data
             if (selection == 'Player Salaries'):
                 get_salary(soup)
+            if (selection == 'Team Roster'):
+                get_roster(soup)
 
             # confirm if user wants to view more of this team's stats
             questions = [{
@@ -75,7 +103,7 @@ def main():
         'type': 'list',
         'name': 'team_name',
         'message': 'What team\'s data would you like to view?',
-        'choices': teams.values()
+        'choices': teams.keys()
         },
         {
         'type': 'list',
@@ -88,7 +116,7 @@ def main():
         'type': 'list',
         'name': 'stat_selection',
         'message': 'Which data would you like to view?',
-        'choices': ['Player Salaries', 'PPG', 'Rebounds']
+        'choices': ['Team Roster', 'Player Salaries']
         }]
     answers = prompt(questions)
     collect_data(
